7462c8864e8275c18c1f6de2bd0d483c51d7340b
aditithuse <aditi.thuse@gmail.com>
2019-02-14 18:22:17 -0800

    Create LengthOfLastWord.cs

---
+++ LengthOfLastWord.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        s=s.TrimEnd();
+        if(s=="")
+            return 0;
+        string[] words = s.Split(' ');
+        return words[words.Length-1].Length;
+    }
+}
