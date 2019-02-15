b4163dcfd58e624f44fe7b0becce52db5d5e06dc
aditithuse <aditi.thuse@gmail.com>
2019-02-15 15:53:09 -0800

    Delete LengthOfLastWord.cs

--- LengthOfLastWord.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int LengthOfLastWord(string s) {
-        s=s.TrimEnd();
-        if(s=="")
-            return 0;
-        string[] words = s.Split(' ');
-        return words[words.Length-1].Length;
-    }
-}
