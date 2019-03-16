65acc002e09cf332661b5c922a665ae0396ad001
aditithuse <aditi.thuse@gmail.com>
2019-03-16 14:21:45 -0700

    Add files via upload

---
+++ Other Problem Solving/ProductElemetsExceptI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ProductElemetsExceptI
+    {
+        public static void Main()
+        {
+            ProductElemetsExceptI.Product(new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        public static int[] Product(int[] arr)
+        {
+            int len = arr.Length;
+            int[] final = new int[arr.Length];
+
+            int[] product_Left = new int[len];
+            int[] product_Right = new int[len];
+
+            product_Left[0] = 1;
+            product_Right[len - 1] = 1;
+
+            for(int i = 1; i < len; i++)
+            {
+                product_Left[i] = product_Left[i - 1] * arr[i - 1];
+            }
+
+            for (int i = len-2; i >=0; i--)
+            {
+                product_Right[i] = product_Right[i + 1] * arr[i + 1];
+            }
+
+            for(int i = 0; i < len; i++)
+            {
+                final[i] = product_Left[i] * product_Right[i];
+            }
+
+            return final;
+
+        }
+    }
+}
---
+++ Other Problem Solving/SumOf2NumbersToK.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SumOf2NumbersToK
+    {
+        public static bool Sum(int[] arr, int k)
+        {
+            if (arr.Length == 0 || arr.Length==1) return false;
+            else
+            { 
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    if (arr.Contains(k - arr[i]))
+                        return true;
+                }
+                
+            }
+            return false;
+        }
+        //public static void Main()
+        //{
+        //    int[] arr = { 10, 15, 3, 9 };
+        //    SumOf2NumbersToK.Sum(arr, 17);
+        //}
+    }
+}
