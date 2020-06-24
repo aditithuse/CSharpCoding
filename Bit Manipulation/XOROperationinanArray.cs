260b8853755288920875a7e8f7b9a18f23c9307d
Aditi Thuse <aditi.thuse@gmail.com>
2020-06-24 11:01:04 -0400

    Update XOROperationinanArray.cs

--- Bit Manipulation/XOROperationinanArray.cs
+++ Bit Manipulation/XOROperationinanArray.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public int XorOperation(int n, int start) {
-        int[] nums=new int[n];
-        nums[0]=start+2*0;
-        int xor=nums[0];
+        int xor=start;
         
         for(int i=1;i<n;i++){
-            nums[i]=start+2*i;
-            xor=nums[i]^xor;            
+            xor^=(start+2*i);
+            
         }
         return xor;
     }
